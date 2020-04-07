3df1be173dc197fe98bbeb7cf480acbd60478f30
dxy2233 <691369338@qq.com>
2020-04-07 14:28:19 +0800

    build confing

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+// const path = require('path')
+
+// function resolve(dir) {
+//   return path.join(__dirname, dir)
+// }
+
+module.exports = {
+  //   outputDir: './../static',
+  productionSourceMap: false,
+  css: {
+    sourceMap: false
+  }
+  // devServer: {
+  //   overlay: {
+  //     warnings: true,
+  //     errors: true
+  //   }
+  // },
+}
